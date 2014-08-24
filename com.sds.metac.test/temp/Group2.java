d6e07ebff603a9e255d498fa809da94eb5f0211c
JUNE <JUNE@JUNE-PC>
2014-08-24 22:47:29 +0900

    왜 한번에 안올라가

---
+++ com.sds.metac.test/temp/Group2.java
@@ -0,0 +1,29 @@
+
+
+/**
+ *  2번그룹 공통코드
+ * 
+ */
+public enum Group2 {
+
+
+    /**
+     *  2번그룹
+     * 
+     */
+    Group2("\ucf54\ub4dc\uac12 : null");
+    /**
+     * 코드에 매핑된 실제 값
+     * 
+     */
+    private String codeValue;
+
+    private Group2(String codeValue) {
+        this.codeValue = codeValue;
+    }
+
+    public String getCodeValue() {
+        return this.codeValue;
+    }
+
+}
