a3be6b75fab8a212e5448b74bde00e62b56bf2e4
Dongjun <Dongjun@dj-kiim>
2014-09-02 10:36:09 +0900

    update

--- com.sds.postjavaprocessor/src/main/java/com/sds/metac/post/CompileJavaFile.java
+++ com.sds.postjavaprocessor/src/main/java/com/sds/metac/post/CompileJavaFile.java
@@ -9,16 +9,21 @@ import javax.tools.JavaFileObject;
 import javax.tools.StandardJavaFileManager;
 import javax.tools.ToolProvider;
 
-public class JavaCompile {
+public class CompileJavaFile {
+	
+	
+	// 소스 위치, 소스목록, 타겟위치 
+	
+	
+	
 	public static void main(String[] args) throws IOException {
 		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
 //		DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
 		DiagnosticCollector<JavaFileObject> diagnostics = null;
-		
 		StandardJavaFileManager fileManager = compiler.getStandardFileManager(diagnostics, null, null);
 		
-//		Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromFiles();
 		Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromStrings(Arrays.asList("./inputfiles/TestClass.java"));
+//		Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromFiles();
 		
 		JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager, diagnostics, null, null, compilationUnits);
 		boolean success = task.call();
